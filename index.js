bfbe2a35d55870aa015964af03ceeac6b2be4ea3
bshirazi <bshirazi@users.noreply.github.com>
2024-06-10 12:08:51 +0330

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 // Spend a minute memorizing the line below
 // Re-write the line of code as best you can from memory
-ReactDOM.render(<p>Hi, my name is Bob!</p>, document.getElementById("root"))
+ReactDOM.render(
+    <ul><li>Thing 1</li><li>Thing 2</li></ul>,
+    document.getElementById("root")
+)
