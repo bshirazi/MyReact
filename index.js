f1599f07c1c34fef0f134144d2b3987b17ffe724
bshirazi <bshirazi@users.noreply.github.com>
2024-06-11 15:31:01 +0330

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,11 @@
-// Spend a minute memorizing the line below
-// Re-write the line of code as best you can from memory
 import React from "react"
 import ReactDOM from "react-dom"
 
-const navbar = (
-    <nav>
-        <h1>Bob's Bistro</h1>
-        <ul>
-            <li>mMenu</li>
-            <li>About</li>
-            <li>Contact</li>
-        </ul>
-    </nav>
+const page = (
+    <div>
+        <img src="./react-logo.png" width="40px" />
+        <h1
+    </div>
 )
-ReactDOM.render(page, document.getElementById("root"))
 
+ReactDOM.render(page, document.getElementById("root"))
