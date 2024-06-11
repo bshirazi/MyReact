d638b0330fd6b3a89445fbdee63422e8020268b9
bshirazi <bshirazi@users.noreply.github.com>
2024-06-11 16:18:22 +0330

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,42 @@
+function Header() {
+    return (
+        <header>
+            <nav>
+              <small>© 2021 Ziroll development. All rights reserved.</small>
+            </nav>
+        </header>
+    )
+}
 
+function Footer() {
+    return (
+        <footer>
+            <small>© 2021 Ziroll development. All rights reserved.</small>
+        </footer>
+    )
+}
+
+function MainContent() {
+    return (
+        <div>
+            <h1>Reasons I'm excited to learn React</h1>
+            <ol>
+                <li>It's a popular library, so I'll be 
+                able to fit in with the cool kids!</li>
+                <li>I'm more likely to get a job as a developer
+                if I know React</li>
+            </ol>
+        </div>
+    )
+}
 
 function Page() {
     return (
-        <ol>
-            <li>It's a popular library, so I'll be 
-            able to fit in with the cool kids!</li>
-            <li>I'm more likely to get a job as a developer
-            if I know React</li>
-        </ol>
+        <div>
+            <Header />
+            <MainContent />
+            <Footer />
+        </div>
     )
 }
 
